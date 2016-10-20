7273a76880e7139f240650dd745dbf7dfc5f32c8
TariqAHassan <tariqaahassan@gmail.com>
2016-10-20 01:50:07 -0300

    Refactor University Information Lookup Procedure

--- ZS_Data_Analysis/ivory_tower_lookup.py
+++ ZS_Data_Analysis/ivory_tower_lookup.py
@@ -1,29 +1,22 @@
-'''
+"""
 
-    This Script Structures the Data Following Extraction from Wikipedia
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    This Extracts Information about Universties from Wikipedia
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     Python 3.5
 
-see :
-  https://github.com/endSly/world-universities-csv
-  https://en.wikipedia.org/wiki/List_of_colloquial_names_for_universities_and_colleges_in_the_United_States
-  Kaggle Data
+    see :
+        https://github.com/endSly/world-universities-csv
+        https://en.wikipedia.org/wiki/List_of_colloquial_names_for_universities_and_colleges_in_the_United_States
+        --> Kaggle Data
 
-
-'''
-
-# ---------------- #
-#  Import Modules  #
-# ---------------- #
-
+"""
+# Import Modules
 import os
 import time
 import numpy as np
 import pandas as pd
 import datetime
-from fuzzywuzzy import fuzz    # needed?
-from fuzzywuzzy import process
 from supplementary_fns import cln
 from supplementary_fns import lprint
 from supplementary_fns import insen_replace
@@ -34,20 +27,11 @@ from supplementary_fns import items_present_test
 
 # ZeitSci Classes
 from zeitsci_wiki_api import WikiUniversities
-# from open_cage_functwionality import ZeitOpenCage
+from my_keys import OPENCAGE_KEY, MAIN_FOLDER
 
-# TO DO: Remove website col.
-
-from my_keys import OPENCAGE_KEY
-MAIN_FOLDER = "/Users/tariq/Google Drive/Programming Projects/ZeitSci/"
-
-# Note: fuzzywuzzy needs python-levenshtein
-#       -- faster...though wdb is ~45000 rows, so search will be slow no matter what.
-
-
-# -------------------- #
-#     Import Data      #
-# -------------------- #
+# ---------------------------------------------------------------- #
+# Import Data   
+# ---------------------------------------------------------------- #
 
 # Import country db
 cdb = pd.read_csv(MAIN_FOLDER + "Data/" + "countries_by_continents.csv", encoding = "ISO-8859-1")
@@ -67,11 +51,9 @@ wdb = pd.read_csv(MAIN_FOLDER + "Data/" + "world_universities.csv", keep_default
 # see wiki: https://en.wikipedia.org/wiki/Template:Most_traded_currencies
 iso_currencies = pd.read_csv(MAIN_FOLDER + "Data/" + "popular_ISO_currencies.csv")
 
-# -------------------------- #
-#  Correct/Add Country Col.  #
-# -------------------------- #
-
-# Use of isodb #
+# ---------------------------------------------------------------- #
+# Correct/Add Country Col.  
+# ---------------------------------------------------------------- #
 
 def alpha2_lookup(alpha):
     """
@@ -79,7 +61,6 @@ def alpha2_lookup(alpha):
     :param alpha: ISO Alpha2 Country Code
     :return: Country Name
     """
-
     # Input check
     if not isinstance(alpha, str):
         try:
@@ -98,7 +79,6 @@ def alpha2_lookup(alpha):
     except:
         return ""
 
-
 # Deploy alpha2_lookup() on wdb's country col.
 wdb.rename(columns = {'Country': 'CountryCode'}, inplace = True)
 wdb["Country"] = ""
@@ -116,11 +96,9 @@ for ac in wldc.CountryCode.unique().tolist():
 # Shift Country Column
 wldc = pandas_col_shift(wldc, "Country")
 
-
-# -------------------------- #
-#          Clean cdb         #
-# -------------------------- #
-
+# ---------------------------------------------------------------- #
+# Clean cdb         
+# ---------------------------------------------------------------- #
 
 # Split into Country and Capital
 cdb.CountryCapital = cdb.CountryCapital.apply(lambda i: [cln(j,1).lstrip().rstrip() for j in i.split("-")])
@@ -131,29 +109,24 @@ cdb["Country"] = cdb.CountryCapital.apply(lambda i: i[0])
 # Capital alone
 cdb["Capital"] = cdb.CountryCapital.apply(lambda i: i[1])
 
+# ---------------------------------------------------------------- #
+# Add Continent Information to wdb 
+# ---------------------------------------------------------------- #
 
-# -------------------------------- #
-# Add Continent Information to wdb #
-# -------------------------------- #
-
-# Use of cdb #
-
+# Use of cdb
 def continent_lookup(country):
     """
 
     :param country: any country in the world
     :return: the continent of a country
     """
-
     # Special Case; the USA has many names
     if country.lower().rstrip().lstrip() in ["united states of america", "united states", "america", "usa"]:
         return "North America"
-
     try:
         return np.array(cdb["Continent"][cln(cdb.Country.str.lower(), 1) == cln(country.lower(), 1)])[0]
     except:
         return ""
-
 
 # Deploy continent_lookup ---> good, but not perfect
 wdb["Continent"] = wdb.Country.apply(continent_lookup)
@@ -161,141 +134,100 @@ wdb["Continent"] = wdb.Country.apply(continent_lookup)
 # Split by comma, for countries that are in more than one continent
 wdb["Continent"] = wdb["Continent"].apply(lambda i: [cln(j, 1).lstrip().rstrip() for j in i.split(",")])
 
-# -------------------------------- #
-#           Clean up wdb           #
-# -------------------------------- #
+# ---------------------------------------------------------------- #
+# Reorder and Add Columns                                             
+# ---------------------------------------------------------------- #
 
+# Reorder
 pref_col_order = ["University", "Country", "Continent", "Website"]
-
 wdb = wdb.reindex_axis(pref_col_order, axis = 1)
-
-# -------------------------------------------------------------------------------------------------------------------- #
-#                                                        Restrict DB                                                   #
-# -------------------------------------------------------------------------------------------------------------------- #
-
-
-# # # Limit to North America for now
-# allowed_continents = ["North America"]
-# wdb = wdb[wdb.Continent.apply(items_present_test, args = [allowed_continents,])]
-#
-# # Further limit to a few Universties for now
-# select_unis = [
-#     "Harvard"
-#     ,"University of Toronto"
-#     ,"Dalhousie University"
-#     ,"McGill University"
-#     ,"Northwestern University"
-#     ,"University of British Columbia"
-#     ,"University of California, San Francisco"
-# ]
-#
-# select_unis = [i.lower() for i in select_unis]
-#
-# # rewrite with fuzzywuzzy partial
-# wdb = wdb[wdb.University.apply(partial_list_match, args = (select_unis,))]
-# wdb.index = range(wdb.shape[0])
-
-# --------------------------------------------------------------------------------------------- #
-
-# Limit to North America
-# wdb = wdb[wdb['Country'].apply(items_present_test, args = [allowed_continents,])].reset_index(drop=True)
-# set([i for s in wdb['Continent'].tolist() for i in s])
-wdb = wdb[wdb['Continent'].map(lambda x: 'Oceania' in x)].reset_index(drop=True)
-
-# Move to Correct folder
-os.chdir(MAIN_FOLDER + "/Data/WikiPull/Oceania/semi_complete")
-
-# ---------------------------------------------------------------- #
-#                                                                  #
-#                     Geoinformation Extraction                    #
-#                                                                  #
-# ---------------------------------------------------------------- #
-
-
-# Pass 1: Crawl wikipedia
-# Pass 2: fill remaining log/lat holes with online API
-
-# Construct needed columns #
 
 # Institution Properties
 wdb['Endowment'] = ""
 wdb['InstitutionType'] = ""
-
-# Geo Information
-wdb['Address'] = ""  # will have to use reverse lookup for this
-
 wdb['lng'] = ""
 wdb['lat'] = ""
-# These should be flipped...
-
 wdb['DataSource'] = ""
 
-file_name = "OceaniaUniversities"
-
 # ---------------------------------------------------------------- #
-#                    1.  Wikipedia Geoinformation                  #
+# Wikipedia Geoinformation                                          
 # ---------------------------------------------------------------- #
 
-# Create an instance of the class
+# Create an instance of the WikiUniversities class
 wikiuni = WikiUniversities(iso_currencies=iso_currencies)
 
-# Get the current dir
-current_dir = os.getcwd()
 
-start = 0
-fail_count = []
+def wiki_uni_extractor(df, save_to, file_name, start=0, continent=None, semi_complete=50, partial_saves=True, verbose=True):
+    """
 
-for row in range(start, wdb.shape[0]):
-    # Breaks
-    if start != 0 and row % 500 == 0:
-        print("Breaking for 3 mins")
-        time.sleep(60*3)  # break for 3 mins
-        print(datetime.datetime.now())
+    Very messy code...but it works.
 
-    # Update
-    print("Processing Row", row, "of", wdb.shape[0], "| University:", wdb['University'][row])
+    :param df:
+    :param save_to:
+    :param file_name:
+    :param start:
+    :param continent:
+    :param semi_complete:
+    :param partial_saves:
+    :return:
+    """
+    # Counter
+    fail_count = list()
 
-    # Look up info on wikipedia
-    # try:
-    lookup_rslt = None
-    lookup_rslt = wikiuni.university_information(wiki_page_title=wdb['University'][row], region=wdb['Country'][row])
+    # Limit by continent on request
+    if isinstance(continent, str):
+        df = df[df['Continent'].map(lambda x: continent in x)].reset_index(drop=True)
 
-    # Check that processing is going as desired
-    if lookup_rslt == None:
-        print("No data extracted for this row")
-        fail_count.append(1)
-        if fail_count[-60:] == [1]*90:
-            print("Consistent Failure to extract data. Breaking...")
-            print(fail_count)
-            # break
+    for row in range(start, df.shape[0]):
+        # Breaks
+        if start != 0 and row % 500 == 0:
+            if verbose: print("Breaking for 3 mins")
+            time.sleep(60 * 3)  # break for 3 mins
+            if verbose: print(datetime.datetime.now())
+        if verbose: print("Processing Row", row, "of", df.shape[0], "| University:", df['University'][row])
 
-    # Add information to wdb
-    if lookup_rslt != None:
-        if len(fail_count) > 0: fail_count = []
+        # Look up info on wikipedia
+        try:
+            lookup_rslt = None
+            lookup_rslt = wikiuni.university_information(wiki_page_title=df['University'][row], region=df['Country'][row])
 
-        wdb.loc[row, "Endowment"]       = lookup_rslt["endowment"]
-        wdb.loc[row, "InstitutionType"] = lookup_rslt["institution_type"]
-        wdb.loc[row, "lng"]             = lookup_rslt["lng"]
-        wdb.loc[row, "lat"]             = lookup_rslt["lat"]
-        wdb.loc[row, "DataSource"]      = "Wikipedia"
+            # Check that processing is going as desired
+            if lookup_rslt == None:
+                if verbose: print("No data extracted for this row.")
+                fail_count.append(1)
+                if fail_count[-60:] == [1] * 90:
+                    if verbose: print("Consistent Failure to extract data. Breaking...", "fail count: ", fail_count)
+                    break
 
-    # except:
-    #     print("problem here:", row)
+            # Add information to df
+            if lookup_rslt != None:
+                if len(fail_count) > 0: fail_count = list()
 
-    # Save to disk every 100 rows
-    if row != 0 and row % 50 == 0:
-        print(" ------------ Saving ------------ ")
-        csv_name = file_name + str(time.time()).replace(".", "_") + ".csv"
-        wdb.to_csv(csv_name, sep = ",")
-    if row == (wdb.shape[0]-1):
-        wdb.to_csv(file_name+"_pcomplete_raw.csv", sep = ",")
+                df.loc[row, "Endowment"] = lookup_rslt["endowment"]
+                df.loc[row, "InstitutionType"] = lookup_rslt["institution_type"]
+                df.loc[row, "lng"] = lookup_rslt["lng"]
+                df.loc[row, "lat"] = lookup_rslt["lat"]
+                df.loc[row, "DataSource"] = "Wikipedia"
+        except:
+            pass
 
+        # Save to disk every x rows
+        if partial_saves and row != 0 and row % semi_complete == 0:
+            if verbose: print(" ------------ Saving ------------ ")
+            csv_name = save_to + "/" + file_name + str(time.time()).replace(".", "_") + ".csv"
+            df.to_csv(csv_name, sep=",")
 
+    return df
 
+# Extract
+file_name = "EuropeanUniversities"
+directory = MAIN_FOLDER + "/Data/WikiPull/Europe/semi_complete"
 
+# Run Extractor
+final_db = wiki_uni_extractor(wdb, save_to=directory, start=550, file_name=file_name, continent='Europe')
 
-
-
+# Save result
+final_db.to_csv(save_to +  "/" + file_name + "_pcomplete_raw.csv", sep = ",")
 
 
 
