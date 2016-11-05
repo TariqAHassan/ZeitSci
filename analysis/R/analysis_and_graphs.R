2ccd1ff2710c7c860655baebfa3d8bce62e8aa2a
TariqAHassan <tariqaahassan@gmail.com>
2016-11-05 18:50:08 -0300

    Update R Analyses and Add Violin Plot

--- analysis/R/analysis_and_graphs.R
+++ analysis/R/analysis_and_graphs.R
@@ -5,121 +5,216 @@
 # Import Modules
 library('ez')
 library('lme4')
+library('plyr')
 library('ggvis')
-library('cowplot')
+library('plotly')
+library('pbapply')
 library('ggplot2')
+library('stringr')
 library('reshape2')
-source("data_sources.R")
-options(scipen=5)
+library('gridExtra')
+options(scipen=10)
 
+# Set seed
+set.seed(100)
 
-# ------------------------------------------------ #
+# ------------------------------------------------ 
+# General Use Functions
+# ------------------------------------------------ 
 
-resid_analysis <- function(data_frame, model){
-    # Note: can be tempermental if lmerTest loaded.
-    model_resids <- data_frame
-    model_resids <- data.frame(Researcher = model_resids[,c("Researcher")])
-    model_resids$resids <- Reduce(c, residuals(model))
-    
-    # Make Residuals Positive
-    counter = 0
-    while (-1 %in% sign(model_resids$resids)) {
-        model_resids$resids <- model_resids$resids + 1
-        counter <- counter + 1
-    }
-    print(paste("A value of", counter, "was added to the residuals"))
-    
-    # Base R Plot Settings
-    par(mfrow = c(1,3))
-    
-    # 1. qq-Plot
-    qqp(residuals(model), "norm")
-    
-    # 2. Boxcox
-    bc  <- with(model_resids, boxcox(resids ~ id)) 
-    bcl <- bc$x[bc$y == max(bc$y)]
-    print(paste("lambda = ", round(bcl,4)))
-    
-    # 3. Hist
-    hist(model_resids$resids, breaks = 40)
-    par(mfrow = c(1,1))
+random_df_rows <- function(data_frame, percent){
+    row_numb <- round(nrow(data_frame) * (percent/100))
+    subsection <- data_frame[sample(nrow(data_frame), row_numb),]
+    return(subsection)
 }
 
-# From http://www.cookbook-r.com/Graphs/Plotting_means_and_error_bars_(ggplot2)/
-source("r_cookBook_helper_fns.R")
+# Define a permutation test
+permutation_test <- function(vector_a, vector_b, rep_num=250, func=mean){
+    #' @param vector_a: subsection 1
+    #' @param vector_b: subsection 2
+    #' @param rep_num: number of replications
+    #' @param func: descriptive stat
+    
+    # Get the means
+    vector_a_mean <- mean(vector_a) 
+    vector_b_mean <- mean(vector_b)
+    
+    # Compute the mean difference
+    mean_diff <- vector_b_mean - vector_a_mean
+    
+    # Combine
+    both_types <- c(vector_a, vector_b)
+    
+    # Replicate
+    rand_test <- pbreplicate(rep_num, sample(x = both_types))
+    
+    # Break
+    vector_a_random <- rand_test[1:length(vector_a),]
+    vector_b_random <- rand_test[(length(vector_a)+1):nrow(rand_test),]
+    
+    # Compute the differences
+    mean_diff_vector <- pbapply(X = vector_a_random, MARGIN = 2, FUN = func) - pbapply(X = vector_b_random, MARGIN = 2, FUN = func)
+    
+    # Compute a p-value
+    p <- (sum(abs(mean_diff_vector) >= mean_diff)) / rep_num
+    
+    return(list(p, mean_diff, mean_diff_vector))
+}
+
+# Test
+# permutation_test(rnorm(100000), rnorm(100000))[[1]] # 1
 
 # ------------------------------------------------ #
 setwd("~/Google Drive/Programming Projects/ZeitSci/analysis/R")
-setwd(paste0(MAIN_FOLDER(), "R"))
 
 # Read in Data
 df <- read.csv("funding_data.csv", na.strings=c("","NA", "NAN", 'nan', 'NaN'))
 
 # Strange
-df <- df[df$Grant > 0 & df$GrantYear <= 2015, ]
+df <- df[df$GrantYear >= 2000 & df$GrantYear <= 2015,]
+df <- df[df$NormalizedAmount > 0,]
 
 # Conversions
 df['Grant'] <- as.numeric(df$Grant)
 df['Endowment'] <- as.numeric(df$Endowment)
 
-# ------------------------------------------------ #
-# Linear Mixed Effects Model of Grants
-# ------------------------------------------------ #
+# ------------------------------------------------------------------------------------------------------------
+# Break on Public vs. Private
+# ------------------------------------------------------------------------------------------------------------
 
-# DataFrame For Model Building
-df_m <- df[!(is.na(df$InstitutionType)) & !(is.na(df$OrganizationBlock)) & !(is.na(df$Endowment)), ]
+public <- df$NormalizedAmount[df['InstitutionType'] == "Public"]
+private <- df$NormalizedAmount[df['InstitutionType'] == "Private"]
 
-# Convert to Factor
-df_m$InstitutionType <- as.factor(df_m$InstitutionType)
-df_m$OrganizationBlock <- as.factor(df_m$OrganizationBlock)
+# Drop NAs
+public <- public[!is.na(public)]
+private <- private[!is.na(private)]
+
+# Check Dist of Public
+qqnorm(public); qqline(public)
+hist(public, xlim = c(0, 1000000), breaks = 100000)
+
+# Check Dist of Private
+qqnorm(private); qqline(private)
+hist(private, xlim = c(0, 1000000), breaks = 50000)
+
+# Absolutely not normal.
+# Try using log data (i.e., the natural logarithm) to correct for this seemingly expoential dist.
+
+qqnorm(log(public)); qqline(log(public))
+qqnorm(log(private)); qqline(log(private))
+
+# Better, but the tails are still off here. 
+
+# --------------------------------------------------
+# Permutation Test for Institution Type
+# --------------------------------------------------
+
+# Abandon t-test and use a nonparametric method: the randomization test.
+# Are these two groups different w.r.t. the amount of funding they receive on average?
+
+# Set alpha level
+alpha_value = 0.025
+
+# Run a permutation test
+per_test_institution_type <- permutation_test(public, private)
+
+# Plot
+hist(per_test_institution_type[[3]], breaks = 50)
+abline(v=per_test_institution_type[[2]], col = "red") # the bar is so far to the right it doesn't even show up...
+
+p <- per_test_institution_type[[1]]
+
+p < alpha_value # TRUE
+# Fail to reject H0
+
+# ---------------------------
+# Violin Plot
+# ---------------------------
+
+df_violinplot <- df[!(is.na(df$InstitutionType)),]
+
+# funding_violins_block("Canada", 95)
+# funding_violins_block("United States", 95)
+# funding_violins_block("Europe", 95)
+
+vector_quant <- function(num_vector, q){
+    #' @parm num_vector: vector of numerics
+    #' @parm q: quantile (0-100)
+    quant <- Reduce(c, quantile(num_vector, probs = c(q/100))[paste0(q, "%")])
+    return(quant)
+}
+
+funding_violins_general <- function(data_frame, q, title="", x_label="", y_label="", scale_by=10^6){
+    #' @parm block: funding block in the dataset
+    #' @parm q: quantile (0-100)
+    quant <- vector_quant(data_frame$NormalizedAmount, q); print(paste0(q, "% quantile: ", round(quant, 2)))
+    plotting_data <- data_frame[data_frame$NormalizedAmount <= quant,]
+    plotting_data$NormalizedAmount <- plotting_data$NormalizedAmount/(scale_by)
+    graph <- ggplot(plotting_data, aes(x = InstitutionType, y = NormalizedAmount, fill = InstitutionType)) +
+        geom_violin() +
+        xlab(x_label) +
+        ylab(y_label) +
+        theme_minimal() +
+        scale_fill_manual(values=c("#a157db", "#5770db")) + 
+        stat_summary(fun.y=mean
+                     , geom='point'
+                     , colour = "#333333"
+                     , size = 3
+                     , position = position_dodge(width = 0.9)) +
+        theme(
+            legend.position="none",
+            panel.grid.major.x = element_blank(),
+            panel.grid.minor.x = element_blank() # remove?
+        )
+    return(graph)
+}
+
+# 95% percentile for a grant = $738726.30 USD
+
+agency_type_plot <- funding_violins_general(df_violinplot
+                                            , q = 95
+                                            , x_label = "Type of Institution"
+                                            , y_label = "Value of Grants (100000x USD)"
+                                            , scale_by = 10^5)
+agency_type_plot
 
 
-# Construct a General Model 
-m <- lmer(Grant ~ GrantYear + OrganizationBlock + InstitutionType + (1|Researcher),
-           data = df_m,
-           REML = FALSE)
+a <- ggplotly(agency_type_plot, tooltip = c("density")) %>%
+    config(displayModeBar = F)
 
-# Execute a drop1() on the Model.
-# Precondition prior to computing this model: AIC will be used to guide model selection.
-drop1(m, test = "Chisq")
-
-# Plot Residuals
-hist(residuals(m), breaks=10)
-
-# Get Summary
-AIC(m)
-summary(m)
+# Saving without displayModeBar:
+# config(ggplotly(), displaylogo = FALSE, modeBarButtonsToRemove = list('Pan'))
+htmlwidgets::saveWidget(a, "insitution_type.html")
 
 # --------------------------------------------------------
 # Plots - Exploratory
 # --------------------------------------------------------
 
-df_plotting <- df[!(is.na(df$Grant)),]
+df_plotting <- df[!(is.na(df$GrantYear)) & !(is.na(df$FunderBlock)),]
 
 # Grant Density
 
-ggplot(df_plotting, aes(Grant)) +
-    geom_density(aes(fill = OrganizationBlock, color = OrganizationBlock), alpha = 0.25, na.rm=TRUE) +
-    scale_x_continuous(limits=c(0, 750000), breaks = seq(0, 750000, 150000), expand = c(0, 0)) +
-    scale_y_continuous(limits=c(0, 0.00005), expand = c(0, 0)) +
-    ylab("Density") +
-    xlab("Grants in Real USD (2015)") +
+ggplot(df_plotting[df_plotting$NormalizedAmount < (2.5*10^5),], aes(x=FunderBlock, y=NormalizedAmount)) +
+    geom_violin(aes(fill = FunderBlock), na.rm=TRUE) +
+    # scale_x_continuous(limits=c(0, 750000), breaks = seq(0, 750000, 150000), expand = c(0, 0)) +
+    # scale_y_continuous(limits=c(0, 0.00005), expand = c(0, 0)) +
+    ylab("Value of Grants (USD)") +
+    xlab("") +
     labs(color = "Funding Block", fill = "Funding Block") + 
     theme_bw() +
     theme(
-          axis.line = element_line(colour = "black"),
-          panel.grid.major = element_blank(),
-          panel.border = element_blank(),
-          axis.ticks=element_blank(),
-          panel.background = element_blank()
+        axis.line = element_line(colour = "black"),
+        panel.grid.major = element_blank(),
+        panel.border = element_blank(),
+        axis.ticks=element_blank(),
+        panel.grid.major.x = element_blank(),
+        panel.grid.minor.x = element_blank(),
+        panel.background = element_blank()
     ) 
 
 # ---------------------------
-# Break on Public Vs. Private
-# ---------------------------
-
-# Data on ~ 56% of Entries in the Database
-nrow(df[!(is.na(df$InstitutionType)),]) / nrow(df[(is.na(df$InstitutionType)),])
+# Institution Type data on ~ 65% of Entries in the Database
+nrow(df[!(is.na(df$InstitutionType)),]) / nrow(df)
 # ---------------------------
 
 # Over time -- From 2007 and on because of Small N in 2000-2007
@@ -146,7 +241,7 @@ ggplot(agg_inst, aes(x = Year, y = metric, fill = InstitutionType, color = Insti
 # Break on Block
 # ---------------------------
 
-agg_block <- data.frame(aggregate(round(Grant, 2) ~ GrantYear + OrganizationBlock
+agg_block <- data.frame(aggregate(round(Grant, 2) ~ GrantYear + FunderBlock
                                   , data = df[df$GrantYear >= 2010 & df$GrantYear < 2016,]
                                   , FUN = 'sum'))
 colnames(agg_block) <- c('Year', 'OrganizationBlock', 'Grant')
@@ -162,10 +257,8 @@ ggplot(agg_block, aes(x = factor(Year), y = Grant, fill = OrganizationBlock)) +
     geom_bar(stat = "identity", position = 'dodge', alpha = 0.85) + 
     ylab("Grants (Billions of USD)") +
     labs(fill = "Funding Block") + 
-    scale_y_continuous(breaks = breaks, labels = formatted_labels, expand = c(0,0)) +
+    scale_y_continuous(breaks = breaks, labels = formatted_labels, expand = c(0, 0)) +
     scale_x_discrete(expand = c(0,0))
-
-
 
 
 
