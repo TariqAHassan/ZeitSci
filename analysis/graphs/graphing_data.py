ec2be0206915df92777491a9a5143624b71f1e01
TariqAHassan <tariqaahassan@gmail.com>
2016-11-07 22:56:25 -0400

    Add UK Funding Agencies and Reformat

--- analysis/graphs/graphing_data.py
+++ analysis/graphs/graphing_data.py
@@ -1,21 +1,39 @@
 funders_dict = {
-       'DOD_CDMRP'           : ['US Department of Defence (DOD)', [38.870833, -77.055833], "#5fdb57"]
-     , 'DOD_CNRM'            : ['US Department of Defence (DOD)', [38.870833, -77.055833], "#5fdb57"]
-     , 'DoD_CDMRP'           : ['US Department of Defence (DOD)', [38.870833, -77.055833], "#5fdb57"]
-     , 'DoD_CNRM'            : ['US Department of Defence (DOD)', [38.870833, -77.055833], "#5fdb57"]
+     # Canada
+       'NSERC' : ['Natural Sciences and Engineering Research Council of Canada (NSERC)', [45.418267, -75.703119], "#dbc257"]
+
+     # Europe
      , 'European Commission' : ['European Commission/EU (EC)', [50.843611, 4.382777], "#57dbb2"]
-     , 'HHS_ACF'             : ['US Administration for Children & Families (ACF)', [38.886666, -77.014444], "#57db80"]
-     , 'HHS_CDC'             : ['US Centers for Disease Control and Prevention (CDC)', [33.798817, -84.325598], "#db9057"]
-     , 'HHS_FDA'             : ['US Food and Drug Administration (FDA)', [39.03556, -76.98271], "#d357db"]
-     , 'HHS_NIDILRR'         : ['US National Institute on Disability, Independent Living, and Rehabilitation Research (NIDILRR)',
+
+     # United Kingdom
+     , 'UK_BBSRC' : ['Research Councils (RC UK)', [51.567020, -1.784722], "#5770db"]
+     , 'UK_EPSRC' : ['Research Councils (RC UK)', [51.567020, -1.784722], "#5770db"]
+     , 'UK_MRC'   : ['Research Councils (RC UK)', [51.567020, -1.784722], "#5770db"]
+     , 'UK_NC3Rs' : ['Research Councils (RC UK)', [51.567020, -1.784722], "#5770db"]
+     , 'UK_NERC'  : ['Research Councils (RC UK)', [51.567020, -1.784722], "#5770db"]
+     , 'UK_STFC'  : ['Research Councils (RC UK)', [51.567020, -1.784722], "#5770db"]
+
+     # United States
+     , 'DOD_CDMRP'   : ['US Department of Defence (DOD)', [38.870833, -77.055833], "#5fdb57"]
+     , 'DOD_CNRM'    : ['US Department of Defence (DOD)', [38.870833, -77.055833], "#5fdb57"]
+     , 'DoD_CDMRP'   : ['US Department of Defence (DOD)', [38.870833, -77.055833], "#5fdb57"]
+     , 'DoD_CNRM'    : ['US Department of Defence (DOD)', [38.870833, -77.055833], "#5fdb57"]
+     , 'HHS_ACF'     : ['US Administration for Children & Families (ACF)', [38.886666, -77.014444], "#57db80"]
+     , 'HHS_CDC'     : ['US Centers for Disease Control and Prevention (CDC)', [33.798817, -84.325598], "#db9057"]
+     , 'HHS_FDA'     : ['US Food and Drug Administration (FDA)', [39.03556, -76.98271], "#d357db"]
+     , 'HHS_NIDILRR' : ['US National Institute on Disability, Independent Living, and Rehabilitation Research (NIDILRR)',
                                                                                     [38.885939, -77.016469], "#91db57"]
-     , 'HHS_NIH'             : ['US National Institutes of Health (NIH)', [39.000443, -77.102394], "#57a2db"]
-     , 'NASA'                : ['US National Aeronautics and Space Administration (NASA)', [38.883, -77.0163], "#57d3db"]
-     , 'NSERC'               : ['Natural Sciences and Engineering Research Council of Canada (NSERC)',
-                                                                                    [45.418267, -75.703119], "#dbc257"]
-     , 'NSF'                 : ['US National Science Foundation (NSF)', [38.88054, -77.110962], "#db5f57"]
-     , 'USDA_ARS'            : ['US Department of Agriculture: Agricultural Research Service (ARS)',
+     , 'HHS_NIH'     : ['US National Institutes of Health (NIH)', [39.000443, -77.102394], "#57a2db"]
+     , 'NASA'        : ['US National Aeronautics and Space Administration (NASA)', [38.883, -77.0163], "#57d3db"]
+     , 'NSF'         : ['US National Science Foundation (NSF)', [38.88054, -77.110962], "#db5f57"]
+     , 'USDA_ARS'    : ['US Department of Agriculture: Agricultural Research Service (ARS)',
                                                                                     [38.886767, -77.030001], "#db5780"]
-     , 'USDA_FS'             : ['US Forest Service (FS)', [38.886767, -77.030001], "#c3db57"]
-     , 'USDA_NIFA'           : ['US National Institute of Food and Agriculture (NIFA)', [38.886767, -77.030001], "#6f57db"]
+     , 'USDA_FS'     : ['US Forest Service (FS)', [38.886767, -77.030001], "#c3db57"]
+     , 'USDA_NIFA'   : ['US National Institute of Food and Agriculture (NIFA)', [38.886767, -77.030001], "#6f57db"]
 }
+
+
+
+
+
+
